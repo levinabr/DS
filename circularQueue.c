5a811dd3f0843dec2ebd6313b2aae48242b68ca0
Levin Abraham Jacob <levinabrahamjacob@gmail.com>
2025-08-25 20:15:28 +0530

    circularQueue.c

--- circularQueue.c
+++ circularQueue.c
@@ -1,40 +1,83 @@
-#include <stdio.h>
-#define SIZE 5  
-
-int queue[SIZE];
-int front = -1;
-int rear = -1;
-int isFull() {
-    return (front == (rear + 1) % SIZE);
-}
-
-int isEmpty() {
-    return front == -1;
-}
-
-
-void enqueue(int value) {
-    if (isFull()) {
-        printf("Queue Overflow! Cannot enqueue %d\n", value);
-        return;
-    }
-
-    if (isEmpty()) {
-        front = rear = 0;
-    } else {
-        rear = (rear + 1) % SIZE;
-    }
-
-    queue[rear] = value;
-    printf("Enqueued: %d\n", value);
-}
-
-int dequeue() {
-    if (isEmpty()) {
-        printf("Queue Underflow! Cannot dequeue\n");
-        return -1;
-    }
-
+#include<stdio.h>
+#include<stdlib.h>
+#define max 5
+int queue[max];
+int front=-1,rear=-1;
+void enqueue(int value)
+{ 
+  if((front==0&&rear==max-1)||(rear+1)%max==front)
+  {
+    printf("Queue is full");
+    return ;
+  }
+  if(front==-1)
+  {
+    front=rear=0;
+   } 
+   else{
+   rear=(rear+1)%max;
+   }
+   queue[rear]=value;
+  }
+void dequeue()
+{
+  if(front==-1) 
+  {
+    printf("Queue is empty ");
+    return ;
+  }
+  printf("Deleted :%d",queue[front]);
+  if(front==rear)
+  {
+    front=rear=-1;
+  }
+  else
+  {
+    front=(front+1)%max;}
+   }
+   void display()
+   {
+   if(front==-1){
+   printf("Queue is empty ");
+   return ;}
+   printf("Queue :");
+   int i=front;
+   while(1){
+   printf("%d",queue[i]);
+   if(i==rear)
+   {
+   break;}
+   i=(i+1)%max;
+   }printf("\n");}
+   int main(){
+   int choice,value;
+   while(1)
+   {
+     printf("\n ---Circular Queue---\n");
+     printf("1.Enqueue\n2.Dequeue\n3.Display\n4.Exit");
+     printf("\nEnter your choice:");
+     scanf("%d",&choice);
+     switch(choice){
+     case 1:
+          printf("Enter the value:");
+          scanf("%d",&value);
+          enqueue(value);
+          break;
+    case 2:
+          dequeue();
+          break;
+    case 3:
+          display();
+          break;
+    case 4:
+          exit(0);
+          break;
+    default:
+          printf("Invalid Choice");
+          }
+         }
+         return 0;
+   }
     int item = queue[front];
 
     if (front == rear) {

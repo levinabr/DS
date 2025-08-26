9da2d927b775d87f291f2fb65f101cce7d0c6789
Levin Abraham Jacob <levinabrahamjacob@gmail.com>
2025-08-26 09:23:17 +0530

    infixpostfix.c

--- infixpostfix.c
+++ infixpostfix.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <ctype.h>  // for isalpha(), isdigit()
 #include <string.h>
 
 #define MAX 100
@@ -6,256 +7,76 @@
 char stack[MAX];
 int top = -1;
 
-void push(char c) 
-{
-    if (top == MAX - 1) 
-    {
-        printf("Stack Overflow\n");
-    } else {
-        stack[++top] = c;
-    }
+
+void push(char c) {
+    stack[++top] = c;
 }
 
 
-char pop() 
-{
-    if (top == -1) 
-    {
-        return '\0';#include <stdio.h>
-#include <string.h>
-
-#define MAX 100
-
-char stack[MAX];
-int top = -1;
-
-void push(char c) 
-{
-    if (top == MAX - 1) 
-    {
-        printf("Stack Overflow\n");
-    } else {
-        stack[++top] = c;
-    }
-}
-char pop() 
-{
-    if (top == -1) 
-    {
-        return '\0';
-    } else {
+char pop() {
+    if (top == -1)
+        return -1;
+    else
         return stack[top--];
-    }
 }
 
 
-char peek() 
-{
-    if (top == -1) 
-    {
-        return '\0';
-    } else {
-        return stack[top];
-    }
-}
-
-int precedence(char op) 
-{
-    if (op == '^')
+int precedence(char c) {
+    if (c == '^')
         return 3;
-    else if (op == '*' || op == '/')
+    else if (c == '*' || c == '/')
         return 2;
-    else if (op == '+' || op == '-')
+    else if (c == '+' || c == '-')
         return 1;
     else
         return 0;
 }
 
-
-int isRightAssociative(char op)
- {
-    return op == '^';
-}
-
-
-int isOperand(char c) 
-{
-    if ((c >= 'a' && c <= 'z') ||
-        (c >= 'A' && c <= 'Z') ||
-        (c >= '0' && c <= '9'))
-        return 1;
-    return 0;
-}
-
-
-void infixToPostfix(char* infix, char* postfix)
- {
+void infixToPostfix(char infix[]) {
+    char postfix[MAX];
     int i, j = 0;
     char c;
-
-    for (i = 0; infix[i] != '\0'; i++)
-     {
+    
+    for (i = 0; infix[i] != '\0'; i++) {
         c = infix[i];
-
-       
-        if (isOperand(c)) 
-        {
-            postfix[j++] = c;
+        
+        if (isalpha(c) || isdigit(c)) {
+            postfix[j++] = c;  // Operand directly to postfix
         }
-   
-        else if (c == '(') 
-        {
+        else if (c == '(') {
             push(c);
         }
-       
-        else if (c == ')') 
-        {
-            while (top != -1 && peek() != '(')
-             {
+        else if (c == ')') {
+            while (top != -1 && stack[top] != '(') {
                 postfix[j++] = pop();
             }
-            pop(); 
+            pop();  '('
         }
-        
-        else 
-        {
-            while (top != -1 &&
-                   ((precedence(peek()) > precedence(c)) ||
-                   (precedence(peek()) == precedence(c) && !isRightAssociative(c))) &&
-                   peek() != '(') {
+        else { 
+            while (top != -1 && precedence(stack[top]) >= precedence(c)) {
                 postfix[j++] = pop();
             }
             push(c);
         }
     }
-
     
-    while (top != -1)
-     {
+    
+    while (top != -1) {
         postfix[j++] = pop();
     }
-
-    postfix[j] = '\0';
-}
-
-int main()
- {
-    char infix[MAX], postfix[MAX];
-
-    printf("Enter an infix expression: ");
-    scanf("%s", infix);
-
-    infixToPostfix(infix, postfix);
-
-    printf("Postfix expression of the above is: %s\n", postfix);
-
-    return 0;
-}
-    } else {
-        return stack[top--];
-    }
-}
-
-
-char peek() 
-{
-    if (top == -1) 
-    {
-        return '\0';
-    } else {
-        return stack[top];
-    }
-}
-
-int precedence(char op) 
-{
-    if (op == '^')
-        return 3;
-    else if (op == '*' || op == '/')
-        return 2;
-    else if (op == '+' || op == '-')
-        return 1;
-    else
-        return 0;
-}
-
-
-int isRightAssociative(char op)
- {
-    return op == '^';
-}
-
-
-int isOperand(char c) 
-{
-    if ((c >= 'a' && c <= 'z') ||
-        (c >= 'A' && c <= 'Z') ||
-        (c >= '0' && c <= '9'))
-        return 1;
-    return 0;
-}
-
-
-void infixToPostfix(char* infix, char* postfix)
- {
-    int i, j = 0;
-    char c;
-
-    for (i = 0; infix[i] != '\0'; i++)
-     {
-        c = infix[i];
-
-       
-        if (isOperand(c)) 
-        {
-            postfix[j++] = c;
-        }
-   
-        else if (c == '(') 
-        {
-            push(c);
-        }
-       
-        else if (c == ')') 
-        {
-            while (top != -1 && peek() != '(')
-             {
-                postfix[j++] = pop();
-            }
-            pop(); 
-        }
-        
-        else 
-        {
-            while (top != -1 &&
-                   ((precedence(peek()) > precedence(c)) ||
-                   (precedence(peek()) == precedence(c) && !isRightAssociative(c))) &&
-                   peek() != '(') {
-                postfix[j++] = pop();
-            }
-            push(c);
-        }
-    }
-
     
-    while (top != -1)
-     {
-        postfix[j++] = pop();
-    }
-
-    postfix[j] = '\0';
+    postfix[j] = '\0';  // End postfix expression
+    
+    printf("Postfix Expression: %s\n", postfix);
 }
 
-int main()
- {
-    char infix[MAX], postfix[MAX];
-
-    printf("Enter an infix expression: ");
+int main() {
+    char infix[MAX];
+    
+    printf("Enter an Infix Expression: ");
     scanf("%s", infix);
-
-    infixToPostfix(infix, postfix);
-
-    printf("Postfix expression of the above is: %s\n", postfix);
-
+    
+    infixToPostfix(infix);
+    
     return 0;
-}
+} 
